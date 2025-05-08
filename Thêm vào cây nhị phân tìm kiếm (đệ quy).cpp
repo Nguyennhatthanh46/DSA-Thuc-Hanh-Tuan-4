93d9ecdf045dbb5476930e2794aa45f527b01d3a
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2025-05-09 06:25:50 +0700

    Thêm vào cây nhị phân tìm kiếm (đệ quy).cpp

---
+++ Thêm vào cây nhị phân tìm kiếm (đệ quy).cpp
@@ -0,0 +1,94 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+std
+###End banned keyword*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+	}
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    Node * insert(Node * root, int data) {
+//###INSERT CODE HERE
+Node* T = root;
+if(!T){
+    Node* tmp = new Node(data);
+    return tmp;
+}
+else{
+
+            if(data <= root->data){
+                T = insert(root->left,data);
+                root->left = T;
+            }else{
+                T = insert(root->right,data);
+                root->right = T;
+            }
+        }
+        return root;
+    }
+}; 
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.preOrder(root);
+
+    return 0;
+}
